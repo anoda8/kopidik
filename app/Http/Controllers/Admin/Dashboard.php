e2137b962862421f0953b55ac20e3001ba4fa18a
anoda8 <nosada29@gmail.com>
2022-11-19 00:42:17 +0700

    dashboard counter

--- app/Http/Controllers/Admin/Dashboard.php
+++ app/Http/Controllers/Admin/Dashboard.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\DataPesertaDidik;
+use Livewire\Component;
 
-class Dashboard extends Controller
+class Dashboard extends Component
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    public function render()
     {
-        return view('admin.dashboard');
+        $serdik = ['jum_serdik' => $this->counterSerDik()];
+        return view('admin.dashboard', $serdik);
+    }
+
+    public function counterSerDik()
+    {
+        return DataPesertaDidik::get()->count();
     }
 }
